5a4041e8ec4d93a2245799aa6ce38a01005e0300
Ugochukwu Nebolisa <nebolisaugochukwu@gmail.com>
2025-11-10 21:16:52 +0100

    chore(deps): add dotenv to load environmental variables

--- nest/src/app.module.ts
+++ nest/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
-import { PrismaModule } from "./prisma/prisma.module";
+import { ConfigModule } from "@nestjs/config";
 
 @Module({
-  imports: [PrismaModule],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })

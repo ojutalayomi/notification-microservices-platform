b0d0695014e2487c32203999a7a11e70bac966fe
Ugochukwu Nebolisa <nebolisaugochukwu@gmail.com>
2025-11-12 17:18:52 +0100

    refactor: move the user service folder to the root

--- user-service/src/main.ts
+++ user-service/src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
   );
   app.setGlobalPrefix("api/v1");
   app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
